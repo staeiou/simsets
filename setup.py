a4f42dfb7b2f3110c7e648b7b3f6f05306a9a390
Stuart Geiger <sgeiger@gmail.com>
2017-02-10 14:24:54 -0800

    v 0.0.4

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='simsets',
+      version='0.0.4',
+      description='Create simulated datasets from original datasets',
+      url='http://github.com/staeiou/simsets',
+      author='Stuart Geiger',
+      author_email='stuart@stuartgeiger.com',
+      license='MIT',
+      packages=['simsets'],
+      install_requires=[
+          'numpy', 'pandas'],
+      zip_safe=False)
---
+++ simsets/__init__.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import numpy
+import pandas as pd
+
+def simulate_dataset(dataframe, length = 100):
+    simulated = dataframe[:0]
+    for count in range(0,length):
+        row = {}
+        for variable, values in dataframe.items():
+            row[variable]=numpy.random.choice(values)
+        simulated.loc[count]= pd.Series(row)
+
+        return simulated
